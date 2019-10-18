dd51c2703332c7a9bf639b4b4fd5f88a2000411e
Ronald Connal <ron.connal@gmail.com>
2019-10-18 13:14:22 -0700

    Update - random color and clear
    Picking a color from a them and clearing tiles ....

--- src/index.js
+++ src/index.js
@@ -34,16 +34,16 @@ function createData(rowNum, columnNum) {
   return data;
 }
 
-function updateData(data, rowNum, columnNum, color) {
+function updateData(data, rowNum, columnNum) {
+  const theme = ["blie", "green", "red", "orange"];
+  const idx = Math.floor(Math.random() * theme.length);
   const newData = data.map(subarray => [...subarray]);
-  newData[rowNum][columnNum] = color;
+  newData[rowNum][columnNum] = theme[idx];
   return newData;
 }
 
 function Board(props) {
-  const rows = createRow(props.rows);
-  const columns = createRow(props.columns);
-  const [colors, setColors] = useState(createData(props.rows, props.columns));
+  const { rows, columns, colors, setColors } = props;
   return (
     <div className="container">
       {columns.map((_, columnIndx) => (
@@ -52,7 +52,7 @@ function Board(props) {
             <Tile
               color={colors[rowIndx][columnIndx]}
               onClick={() => {
-                const newColor = updateData(colors, rowIndx, columnIndx, "red");
+                const newColor = updateData(colors, rowIndx, columnIndx);
                 console.log(columnIndx, rowIndx);
                 setColors(newColor);
               }}
@@ -65,7 +65,26 @@ function Board(props) {
 }
 
 function App() {
-  return <Board rows={3} columns={4} />;
+  const rows = createRow(3);
+  const columns = createRow(4);
+  const [colors, setColors] = useState(createData(3, 4));
+  return (
+    <div>
+      <Board
+        rows={rows}
+        columns={columns}
+        colors={colors}
+        setColors={setColors}
+      />
+      <button
+        onClick={() => {
+          setColors(createData(3, 4));
+        }}
+      >
+        Clear
+      </button>
+    </div>
+  );
 }
 
 const rootElement = document.getElementById("root");
